c00aac36bcf965a63a17c200ea256f4a7b2c6d0e
parvisharma8834-droid <parvisharma8834@gmail.com>
2026-04-08 16:57:24 +0530

    firstcommit

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -12,7 +12,7 @@ import { test, expect } from '@playwright/test';
   await expect(page.locator("//h2[text()='Login to your account']")).toBeVisible();
 
   await page.locator('input[placeholder="Email Address"]').first().fill('shivanigupta40392@gmail.com');
-  await page.locator('input[placeholder="Passwo"]').first().fill('Shivani40392@');
+  await page.locator('input[placeholder="Password"]').first().fill('Shivani40392@');
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page.getByText('Shivani')).toBeVisible();
      await page.locator('a[href="/logout"]').click();
